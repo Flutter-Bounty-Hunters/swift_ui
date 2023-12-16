827809a58be760031d495380bf4b1aaa9818fc98
Maatteogekko <matteogent9991@gmail.com>
2023-12-16 20:20:03 +0100

    add examples

--- doc_swift_ui_gallery/swift_ui_gallery/swift_ui_gallery/primitives/text/localization/TextLocalizationPage.swift
+++ doc_swift_ui_gallery/swift_ui_gallery/swift_ui_gallery/primitives/text/localization/TextLocalizationPage.swift
@@ -1,26 +1,49 @@
 import SwiftUI
 
 struct TextLocalizationPage: View {
-    var someString = "pen"
-    var object = LocalizedStringResource("pencil")
     
     var body: some View {
         ScrollView {
             VStack(spacing: 24) {
-                // Localized if possible
+                // Text's initializer accepts a LocalizedStringKey, which can be implicitly constructed from a string literal
+                // The value of the string is used for the development language localization
+                // Values for other languages are looked up in the default table using the string as key, if not found the value is used as is
                 Text("pencil")
                 
-                // Variables not localized by default
+                // The comment is shown in the localization table, useful for giving context to translators
+                Text("another pencil", comment: "Translator comment")
+                
+                // This comment will be ignored, since the same key was declared earlier. This is valid even if no comment was specified on the previous key.
+                Text("another pencil", comment: "A different comment")
+                
+                // Specify a different table for this localization
+                Text("pencil", tableName: "LocalizableAlternative")
+                
+                // Specify a bundle that contains the localizations table (Bundle.main is the default)
+                Text("pencil", bundle: .main)
+                
+                Divider()
+                
+                // Variables are not localized by default
+                let someString = "pen"
                 Text(someString)
                 
-                // Explicitly localize a variable
+                // Make variables localizable by wrapping them in LocalizableStringKey
                 Text(LocalizedStringKey(someString))
                 
-                // From localized string resource
-                Text(object)
-                
-                // Explicitly not localized
+                // Do not localize a string literal
                 Text(verbatim: "pencil")
+                
+                Divider()
+                
+                // LocalizedStringResource initializer
+                let resource = LocalizedStringResource("pencil")
+                Text(resource)
+                
+                Divider()
+                
+                // Markdown-formatted strings can also be localized
+                Text("_Please visit our [website](https://www.example.com)._")
             }
         }
     }
@@ -28,4 +51,8 @@ struct TextLocalizationPage: View {
 
 #Preview {
     TextLocalizationPage()
+}
+
+#Preview("Italian") {
+    TextLocalizationPage().environment(\.locale, .init(identifier: "it"))
 }
