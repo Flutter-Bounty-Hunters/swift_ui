4379771d475489ad081340b2c5920873c9f8ee5b
Maatteogekko <matteogent9991@gmail.com>
2024-01-28 10:24:20 +0100

    add example

---
+++ doc_swift_ui_gallery/swift_ui_gallery/swift_ui_gallery/primitives/image/localization/ImageLocalizationPage.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+
+struct ImageLocalizationPage: View {
+    @State private var localize: Bool
+
+    init(localize: Bool = false) {
+        _localize = State(initialValue: localize)
+    }
+    
+    var body: some View {
+        Toggle(isOn: $localize, label: {
+            Text("Localize")
+        }).padding()
+        
+        ScrollView {
+            VStack(spacing: 24) {
+                Image("Dash_hello")
+                    .resizable()
+                    .scaledToFit()
+                
+                // If there is no image specified for the current locale, the default one isued, if provided.
+                Image("Dash_hello")
+                    .resizable()
+                    .scaledToFit()
+                    .environment(\.locale, .init(identifier: "ja"))
+            }
+        }.navigationTitle("Localization")
+            .environment(\.locale, $localize.wrappedValue ? .init(identifier: "it") : .current)
+    }
+}
+
+#Preview {
+    ImageLocalizationPage()
+}
+
+#Preview("Italian") {
+    ImageLocalizationPage(localize: true)
+}
